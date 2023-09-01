d5f0c190e013b2af3bcffbb2e6dabc4473f3a5a6
manevardazaryan1 <114493766+manevardazaryan1@users.noreply.github.com>
2023-09-01 10:54:10 -0800

    Update Task4_transformMatrix.js

--- August_Homework3/Task4_transformMatrix.js
+++ August_Homework3/Task4_transformMatrix.js
@@ -6,9 +6,10 @@
 // Շրջել մատրիցան այնպես որ տողերը դառնան սյուներ, իսկ սյուները՝ տողեր։
 
 function transformMatrix(matrix) {
+    const tmp;
     for (let row = 0; row < matrix.length; ++row) {
         for (let col = row; col < matrix[row].length; ++col) {
-            const tmp = matrix[row][col];
+            tmp = matrix[row][col];
             matrix[row][col] = matrix[col][row];
             matrix[col][row] = tmp;
         }
