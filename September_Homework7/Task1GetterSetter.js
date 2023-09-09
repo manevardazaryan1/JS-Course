c114526353da35f0a682ff7eb5162a0564b780e7
Mnae Vardazaryan <manevardazaryan@gmail.com>
2023-09-09 04:21:09 -0800

    Add Homework 7, Task1: getter, setter

---
+++ September_Homework7/Task1GetterSetter.js
@@ -0,0 +1,13 @@
+const obj = {
+    _name: [],
+
+    get name(){
+        return this._name;
+    },
+
+    set name(name){
+        [fname, lname] = name.split(', ');
+        this._name = [[fname, fname.length], [lname, lname.length]];
+    },
+};
+
