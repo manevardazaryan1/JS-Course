f61049c38484a24528a7ea821bb59964d501b0c4
Mnae Vardazaryan <manevardazaryan@gmail.com>
2023-09-21 03:43:10 -0800

    Add Account class

---
+++ September_Homework8/account.js
@@ -0,0 +1,43 @@
+class Account{
+    #balance = 0;
+
+    constructor (name) {
+        this.name = name;
+        this.id = Date.now();
+    }
+
+    get balance() {
+        return this.#balance;
+    }
+
+    set balance(balance) {
+        this.#balance = balance;
+    }
+
+    credit(balance) {
+        this.#balance += balance;
+    }
+
+    debit(balance) {
+        if (this.#balance < balance)
+            return 'There is not enought money in the account';
+
+        this.#balance -= balance;
+
+        return 'The action is done';
+    }
+
+    transferTo(otherAccount, balance) {
+        if (otherAccount.balance < balance)
+            return 'In this account is not enought money';
+
+        otherAccount.balance -= balance;
+        this.#balance += balance;
+
+        return 'The action is done';
+    }
+
+    static identifyAccounts(otherAccount) {
+        return Account.id === otherAccount.id;
+    }
+}
