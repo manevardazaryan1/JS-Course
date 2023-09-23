5af1859807ec93e5ec67f07d2a4abb5d8e23d3a3
Mnae Vardazaryan <manevardazaryan@gmail.com>
2023-09-23 02:46:58 -0800

    Add homework9/ Date/Json/Task monthsInterval

---
+++ September_Homework9/monthsInterval.js
@@ -0,0 +1,23 @@
+function monthsInterval(date1, date2) {
+    const months = ['january', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const month1 = date1.getMonth();
+    const month2 = date2.getMonth();
+    const start = month1 < month2 ? month1: month2;
+    const end = month1 + month2 - start;
+
+    const monthsArray = [];
+
+    for (let month = start; month <= end; ++month){
+        monthsArray.push(months[month]);
+    }
+
+    return monthsArray;
+}
+
+
+let january = new Date(2017, 0, 1); 
+let march = new Date(2017, 2, 1 );
+console.log(monthsInterval(january, march));
+
+
+
