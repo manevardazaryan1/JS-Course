c704a17092bb6d14fec091878b5ab0edbe2f5d64
Mnae Vardazaryan <manevardazaryan@gmail.com>
2023-09-28 06:10:46 -0800

    Change monthsInterval function

--- September_Homework9/monthsInterval.js
+++ September_Homework9/monthsInterval.js
@@ -1,33 +1,32 @@
 function monthsInterval(date1, date2) {
-    const months = ['january', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    const start = date1.getFullYear() < date2.getFullYear() ? date1: date2;
-    const end = date1.getFullYear() > date2.getFullYear() ? date1: date2;
-
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const start = date1 < date2 ? date1 : date2;
+    const end = date1 < date2 ? date2 : date1;
+    
+    const startYear = start.getFullYear();
+    const endYear = end.getFullYear();
     const startMonth = start.getMonth();
     const endMonth = end.getMonth();
 
     const monthsArray = [];
+    let monthsCount = 0;
+    
+    for (let year = startYear; year <= endYear; year++) {
+        const monthStart = (year === startYear) ? startMonth : 0;
+        const monthEnd = (year === endYear) ? endMonth : 11;
+        
+        for (let month = monthStart; month <= monthEnd; month++) {
+            monthsCount += 1
 
-    if (startMonth >= endMonth) {
-        for (let month = startMonth; month < months.length; ++month) {
-            monthsArray.push(months[month]);
-        }
-
-        for (let month = 0; month <= endMonth; ++month) {
-            monthsArray.push(months[month]);
-        }
-    } else {
-        for (let month = start; month <= end; ++month) {
+            if (monthsCount > 12)
+                break;
+                
             monthsArray.push(months[month]);
         }
     }
-    
-    if (monthsArray.length > 12)
-        monthsArray.length -= 1;
 
     return monthsArray;
 }
-
 
 
 
